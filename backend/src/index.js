9a7d90166b6c8a9ae34a348c1597d29f704fc797
Friedrichfschr <friedrich.riesel@gmail.com>
2025-02-01 15:51:51 -0600

    Create Message Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 
 dotenv.config()
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 
 
