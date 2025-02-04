839c7c8fe2ea3199c4c22836642b1150bcf813f4
Friedrichfschr <friedrich.riesel@gmail.com>
2025-02-03 21:08:08 -0600

    error shooting for deploy

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected: " + conn.connection.host)
     } catch (error) {
-        console.log("failed to connect Database ")
+        console.log("failed to connect Database", error)
     }
 };
 
