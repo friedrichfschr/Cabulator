b06fd38487e218dc067d481492c27d9ea65d48de
Friedrichfschr <friedrich.riesel@gmail.com>
2025-02-16 21:32:01 -0500

    bug fix

--- frontend/src/components/messages/ChatContainer.jsx
+++ frontend/src/components/messages/ChatContainer.jsx
@@ -21,7 +21,7 @@ const ChatContainer = () => {
     //     }
     //     getReadStateAsync()
 
-}, [selectedContact, getReadState])
+    // }, [selectedContact, getReadState])
 
     useEffect(() => {
         getMessages(selectedContact._id);
@@ -31,92 +31,92 @@ const ChatContainer = () => {
     }, [selectedContact._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
 
-useEffect(() => {
-    if (messageEndRef.current && messages) {
-        messageEndRef.current.scrollIntoView({ behavior: "smooth" })
+    useEffect(() => {
+        if (messageEndRef.current && messages) {
+            messageEndRef.current.scrollIntoView({ behavior: "smooth" })
+        }
+    }, [messages]);
+
+
+
+    if (isMessagesLoading) {
+        return (
+            <div className='flex flex-col h-full'>
+                <ChatHeader />
+                <MessageSkeleton />
+                <MessageInput />
+            </div>
+        )
     }
-}, [messages]);
 
-
-
-if (isMessagesLoading) {
     return (
-        <div className='flex flex-col h-full'>
-            <ChatHeader />
-            <MessageSkeleton />
-            <MessageInput />
-        </div>
-    )
-}
+        <div className='flex flex-col h-full  '>
+            <div >
+                {/* className='mt-30 max-[450px]:mt-54' */}
+                <ChatHeader />
+            </div>
 
-return (
-    <div className='flex flex-col h-full  '>
-        <div >
-            {/* className='mt-30 max-[450px]:mt-54' */}
-            <ChatHeader />
-        </div>
-
-        {messages.length > 0 ? (
-            <div className='flex-1 overflow-y-auto overflow-x-hidden m-4 space-y-4'>
-                {messages.map((message, index) => {
-                    const isLastMessage = index === messages.length - 1;
-                    return (
-                        <div
-                            key={message._id}
-                            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-                            ref={isLastMessage ? messageEndRef : null}
-                        >
-                            <div className="chat-image avatar">
-                                <div className="size-10 rounded-full border">
-                                    <img
-                                        src={
-                                            message.senderId === authUser._id
-                                                ? authUser.profilePic || "/avatar.png"
-                                                : selectedContact.profilePic || "/avatar.png"
-                                        }
-                                        alt="profile pic"
-                                    />
+            {messages.length > 0 ? (
+                <div className='flex-1 overflow-y-auto overflow-x-hidden m-4 space-y-4'>
+                    {messages.map((message, index) => {
+                        const isLastMessage = index === messages.length - 1;
+                        return (
+                            <div
+                                key={message._id}
+                                className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+                                ref={isLastMessage ? messageEndRef : null}
+                            >
+                                <div className="chat-image avatar">
+                                    <div className="size-10 rounded-full border">
+                                        <img
+                                            src={
+                                                message.senderId === authUser._id
+                                                    ? authUser.profilePic || "/avatar.png"
+                                                    : selectedContact.profilePic || "/avatar.png"
+                                            }
+                                            alt="profile pic"
+                                        />
+                                    </div>
                                 </div>
+
+                                <div className="chat-header mb-1">
+                                    <time className="text-xs opacity-50 ml-1">
+                                        {formatMessageTime(message.createdAt)}
+                                    </time>
+                                </div>
+                                <div className="chat-bubble flex flex-col max-w-120">
+                                    {message.image && (
+                                        <img
+                                            src={message.image}
+                                            alt="Attachment"
+                                            className="min-w-20 max-w-full max-h-90 rounded-md mb-1 mt-1 object-contain"
+                                        />
+                                    )}
+
+                                    {message.text && <p style={{ "wordBreak": "break-all" }}>{message.text}</p>}
+                                </div>
+
+
+
                             </div>
-
-                            <div className="chat-header mb-1">
-                                <time className="text-xs opacity-50 ml-1">
-                                    {formatMessageTime(message.createdAt)}
-                                </time>
-                            </div>
-                            <div className="chat-bubble flex flex-col max-w-120">
-                                {message.image && (
-                                    <img
-                                        src={message.image}
-                                        alt="Attachment"
-                                        className="min-w-20 max-w-full max-h-90 rounded-md mb-1 mt-1 object-contain"
-                                    />
-                                )}
-
-                                {message.text && <p style={{ "wordBreak": "break-all" }}>{message.text}</p>}
-                            </div>
+                        );
+                    })}
+                </div>
+            ) : (
+                <div className='flex-1 flex overflow-y-auto space-y items-center justify-center'>
+                    <span className="badge animate-pulse">Start the Conversation!</span>
+                </div>
+            )}
 
 
-
-                        </div>
-                    );
-                })}
+            <div className='  mt-2'>
+                <MessageInput />
             </div>
-        ) : (
-            <div className='flex-1 flex overflow-y-auto space-y items-center justify-center'>
-                <span className="badge animate-pulse">Start the Conversation!</span>
-            </div>
-        )}
-
-
-        <div className='  mt-2'>
-            <MessageInput />
-        </div>
 
 
 
-    </div >
-)
+        </div >
+    )
 }
 
 export default ChatContainer
